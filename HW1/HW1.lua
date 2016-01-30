fe4ae39601ea24311e341adafa5272a3fe201e8b
tomsilver <tomssilver@gmail.com>
2016-01-30 17:01:09 -0500

    2.1 almost done (super ugly)

--- HW1/HW1.lua
+++ HW1/HW1.lua
@@ -9,16 +9,54 @@ cmd:option('-classifier', 'nb', 'classifier to use')
 
 -- Hyperparameters
 -- ...
+function unitTest()
+	local x = torch.ones(3, 2)
+	x[1][1] = 4
+	x[2][1] = 3
+	x[2][2] = 2
+	x[3][1] = 2
+	local Wt = torch.Tensor(4, 2)
+	local b = torch.ones(1, 2)
+	b[1][2] = -1
+	i=0; Wt:apply(function() i = i+1; return i end)
+	local output = validateModel(Wt:t(), b, x)
+	print (output)
 
-function validateModel(W, b)
-    local f = hdf5.open(opt.datafile, 'r')
-    local validation_input = f:read('valid_input'):all():double()
-    n, nfeat = validation_input:size()   
-    --print(n[1], n[2], nfeat)
-    --print(nclasses)
-    Ans = torch.Tensor(n[1], nclasses)
-    Ans:mm(validation_input, W:t())
+end
 
+function sparseMultiply(A, B)
+	-- A is a sparse tensor with 1-padding
+	-- B is a dense tensor
+	-- Matrix multiplication A*B in the straightforward way
+	numRows = A:size(1)
+	numCols = B:size(2)
+	local output = torch.Tensor(numRows, numCols)
+	for r = 1, numRows do
+		for c = 1, numCols do
+			local dotProd = 0
+			-- dot product of row r in dense A and col c in B
+			for j = 1, A:size(2) do		
+				indexIntoB = A[r][j]-1
+				if indexIntoB == 0 then
+					break
+				else
+					dotProd = dotProd + B[indexIntoB][c]
+				end
+			end
+			output[r][c] = dotProd
+		end
+	end
+	return output
+end
+
+
+
+function validateModel(W, b, x)
+    Ans = sparseMultiply(x, W:t())
+    for r = 1, Ans:size(1) do
+    	Ans[r]:add(b)
+    end
+    return Ans
 end   
     
 
@@ -32,7 +70,9 @@ function main()
 
    local W = torch.DoubleTensor(nclasses, nfeatures)
    local b = torch.DoubleTensor(nclasses)
-   validateModel(W, b)
+   local validation_input = f:read('valid_input'):all():double()
+   print(validateModel(W, b, validation_input))
+   --unitTest()
    -- Train.
 
    -- Test.
