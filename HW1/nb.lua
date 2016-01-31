d147e49c9620f699af6f8df6feac103cf2609072
Ankit Gupta <ankitgupta@college.harvard.edu>
2016-01-31 00:04:06 -0500

    factored out getting counts

--- HW1/nb.lua
+++ HW1/nb.lua
@@ -65,11 +65,13 @@ function validateModel(W, b, x, y)
 end   
     
 
-function naiveBayes(alpha)
+function createCountsMatrix(training_input, training_output)
+	print("     CreateCountsMatrix: Opened Data File")
 	local f = hdf5.open(opt.datafile, 'r')
 	local F = torch.zeros(nfeatures, nclasses)
-	local training_input = f:read('train_input'):all():double()
-	local training_output = f:read('train_output'):all():double()
+	--local training_input = f:read(features_table):all():double()
+	--local training_output = f:read(classes_table):all():double()
+	print("     CreateCountsMatrix: Loaded training data")
 	local train_size = training_input:size()
 	for n = 1, train_size[1] do
 		for j = 1, train_size[2] do
@@ -80,57 +82,63 @@ function naiveBayes(alpha)
 			end
 		end
 	end
+	print("     CreateCountsMatrix: Calculated counts")
+	return F
+end
+
+function naiveBayes(alpha)
+	local f = hdf5.open(opt.datafile, 'r')
+	local training_input = f:read('train_input'):all():double()
+	local training_output = f:read('train_output'):all():double()
+	--[[
+	print("     NaiveBayes: Opened Data File")
+	local f = hdf5.open(opt.datafile, 'r')
+	local F = torch.zeros(nfeatures, nclasses)
+	local training_input = f:read('train_input'):all():double()
+	local training_output = f:read('train_output'):all():double()
+	print("     NaiveBayes: Loaded training data")
+	local train_size = training_input:size()
+	for n = 1, train_size[1] do
+		for j = 1, train_size[2] do
+			feat = training_input[n][j] - 1
+			class = training_output[n]
+			if feat > 0 then
+				F[feat][class] = F[feat][class] + 1
+			end
+		end
+	end
+	print("     NaiveBayes: Calculated counts")
+	--]]
+	local F = createCountsMatrix(training_input, training_output)
 
 	-- Add a small offset for smoothing
-	--alpha = 1
 	F = F + alpha
 
-	-- Now, we row normalize the Tensor
+	-- Now, we column normalize the Tensor
 	sum_of_each_col = torch.sum(F, 1)
-	--reciprocal = torch.cdiv(torch.ones(nclasses), sum_of_each_col)
-	--print(reciprocal:size())
 	p_x_given_y = torch.Tensor(nfeatures, nclasses):zero()
-	for s = 1, F:size()[1] do
-		p_x_given_y[s] = torch.cdiv(F[s] , sum_of_each_col)
+	for n = 1, F:size()[1] do
+		p_x_given_y[n] = torch.cdiv(F[n] , sum_of_each_col)
 	end
-	--print(train_size[1])
-
+	print("     NaiveBayes: Calculated p(x|y)")
 	class_distribution = torch.zeros(nclasses)
 	for n=1, training_output:size()[1] do
 		class = training_output[n]
 		class_distribution[class] = class_distribution[class] + 1
 	end
-	--print(class_distribution, "\n")
-	--print(torch.sum(class_distribution, 1))
 	p_y = torch.div(class_distribution, torch.sum(class_distribution, 1)[1])
-	--print(p_y, "\n")
+	print("     NaiveBayes: Calculated p(y)")
 
 	local W = torch.log(p_x_given_y)
 	local b = torch.log(p_y)
 	local validation_input = f:read('valid_input'):all():double()
 	local validation_output = f:read('valid_output'):all():long()
+	print("     NaiveBayes: Initialized validation parameters")
+
     validation_accuracy = validateModel(W:t(), b, validation_input,validation_output)
-    print("Naive Bayes accuracy", validation_accuracy)
-	--return W, b
+	print("     NaiveBayes: Validation Accuracy:", validation_accuracy)
 
-
-
-
-
-
-
-
-
-
-	--for n = 1, train_size[1], 1000 do
-	--	print(normalized[n])
-	--end
-	--print(F)
-	--print(sum_of_each_row)
-	--print("Training input size", training_input:size(), "\n")
-	--print("Training output size", training_output:size(), "\n")
-	-- initialize count matrix: numFeatures x numClasses
-	
+	return validation_accuracy	
 end
 
 
@@ -138,8 +146,10 @@ function main()
    -- Parse input params
    opt = cmd:parse(arg)
    local f = hdf5.open(opt.datafile, 'r')
+   print("Opened datafile")
    nclasses = f:read('nclasses'):all():long()[1]
    nfeatures = f:read('nfeatures'):all():long()[1]
+   print("Loaded size parameters")
 
    local W = torch.randn(nclasses, nfeatures)
    local b = torch.DoubleTensor(nclasses)
