1c04642f3c06e2a35868742a4a7eb082cb19d352
tomsilver <tomssilver@gmail.com>
2016-02-04 22:56:15 -0500

    implemented hinge loss lr

--- HW1/hinge.lua
+++ HW1/hinge.lua
@@ -1,35 +1,150 @@
+dofile("utils.lua")
+
+function hingeLogisticRegression(training_input, training_output, validation_input, validation_output, nfeatures, nclasses, minibatch_size, eta, lambda, num_epochs)
+	printv("Parameters are:", 2)
+	printv("Eta:", 2)
+	printv(eta, 2)
+	printv("Lambda:", 2)
+	printv(lambda, 2)
+	printv("Minibatch size:", 2)
+	printv(minibatch_size, 2)
+	printv("Number of Epochs", 2)
+	printv(num_epochs, 2)
+
+	return hingeSGD(training_input, training_output, validation_input, validation_output, nfeatures, nclasses, minibatch_size, eta, lambda, num_epochs)
+end
+
 function hingeLoss(W, b, Xs, Ys, lambda)
-	local N = Xs:size()[1]
-	local K = W:size()[1]
-	local softmax_res = softmax(Xs, W, b)
+	local numEntries = Xs:size()[1]
+	local numClasses = W:size()[1]
+
+	local yp = sparseMultiply(Xs, W:t())
+    for r = 1, yp:size(1) do
+    	yp[r]:add(b)
+    end
+
 	local total = 0.0
-	for n = 1, N do
+	for n = 1, numEntries do
 		--true class probability
 		local yc = 0
 		--greatest false class probability
 		local ycp = 0
-		for k = 1, K do
+		for k = 1, numClasses do
 			if (Ys[n] == k) then
-				yc = softmax_res[n][k]
+				yc = yp[n][k]
 			elseif (Ys[n] > ycp) then
 				ycp = Ys[n]
 			end
 		end
-		total = total+math.max(0, 1-(yc + ycp))
+		total = total+math.max(0, 1-(yc-ycp))
 	end
-	return ((-1)*total) + .5*lambda*torch.pow(W,2):sum()
+	return total + lambda*torch.pow(W,2):sum()
 end
 
--- Calculates the gradient of W
---      Will be using stochastic gradient descent with minibatches
--- Inputs:
---       W:           weights (nclasses x nfeatures)
---       Xs:          input features (sparse representation)
--- 		 Ys:          output classes (N x 1)
---       start_index: index of start of minibatch
---       end_index:   index of end of minibach
 function hingeGradient(W, b, Xs, Ys, start_index, end_index)
-	TODO
+	local num_rows_wanted = end_index - start_index + 1
+
+	-- Selects the num_rows_wanted rows that start at start_index
+	local X = Xs:narrow(1,start_index,num_rows_wanted)
+	local Y = Ys:narrow(1,start_index,num_rows_wanted)
+
+	-- Extract parameters
+	local W_size = W:size()
+	local nclasses = W_size[1]
+	local nfeatures = W_size[2]
+
+	-- Initiailize gradient 
+	local W_grad = torch.zeros(nclasses, nfeatures)
+	local b_grad = torch.zeros(nclasses)
+
+	-- yp (y predicted) should be (num_rows_wanted X nclasses)
+	local yp = sparseMultiply(Xs, W)
+    for r = 1, yp:size(1) do
+    	yp[r]:add(b)
+    end
+
+	for n = 1, num_rows_wanted do
+ 		--true class probability
+		local yc = 0
+		--greatest false class probability
+		local ycp = 0
+		local cp = 0
+		for k = 1, nclasses do
+			if (Ys[n] == k) then
+				yc = yp[n][k]
+			elseif (Ys[n] > ycp) then
+				ycp = Ys[n]
+				cp = k
+			end
+		end
+		-- dL/dy
+		dLdy = torch.zeros(nclasses)
+		if yc - ycp <= 1 then
+			dLdy[cp] = 1
+			dLdy[Ys[n]] = -1
+		end
+
+ 		local denseTensor = convertSparseToReal(Xs[n], nfeatures)
+ 		for i = 1, nclasses do
+ 			local tmp = torch.mul(denseTensor, dLdy[i])
+ 			W_grad[i] = W_grad[i] + tmp:div(num_rows_wanted)
+ 		end
+ 		b_grad = b_grad + torch.div(dLdy, num_rows_wanted)
+	end
+
+	return W_grad, b_grad
 end
 
+function hingeSGD(Xs, Ys, validation_input, validation_output, nfeatures, nclasses, minibatch_size, learning_rate, lambda, num_epochs)
+	local testmode = false
 
+	local N = Xs:size()[1]
+	local W = torch.randn(nclasses, nfeatures)
+	local b = torch.randn(nclasses)
+	--local num_epochs = 10
+	W:div(1)
+
+	if testmode == true then
+		N = 10000
+		minibatch_size = 250
+		num_epochs = 50
+	end
+
+	for rep = 1, num_epochs do
+		-- Calculate the loss and validation accuracy
+		printv("SGD: Hinge Loss is", 3)
+		printv(hingeLoss(W, b, Xs, Ys, lambda), 3)
+
+		local validation_accuracy = validateLinearModel(W, b, validation_input,validation_output)
+		
+		printv("SGD: Validation Accuracy is:", 3)
+		printv(validation_accuracy, 3)
+		printv("SGD: Magnitude of W:", 3)
+		printv(torch.abs(W):sum(), 3)
+		printv("SGD: Magnitude of b:", 3)
+		printv(torch.abs(b):sum(), 3)
+
+
+		local counter = 0
+		for index = 1, N, minibatch_size do
+			counter = counter + 1
+			local start_index = index
+			-- don't let the end_index exceed N
+			local end_index = math.min(start_index + minibatch_size - 1, N)
+			local size = end_index - start_index + 1
+			--print(start_index, end_index)
+			local W_grad, b_grad = gradient(W, b, Xs, Ys, start_index, end_index)
+			W = W - (W_grad + torch.mul(W,lambda/N)):mul(learning_rate)
+			b = b - torch.mul(b_grad,learning_rate)
+			if counter % 20 == 0 then
+				print("    Current index:", index)
+				print("    Magnitude of W_grad:", torch.abs(W_grad):sum())
+				print("    Magnitude of W:",torch.abs(W):sum())
+				print("    Magnitude of B_grad:", torch.abs(b_grad):sum())
+				print("    Magnitude of b:", torch.abs(b):sum())
+				print("\n")
+			end
+		end
+	end
+	return W, b
+end
