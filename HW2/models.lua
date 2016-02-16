90bca5854c6f95628ad6bebb8b46aeee6934c9aa
Ankit Gupta <ankitgupta@college.harvard.edu>
2016-02-15 19:57:37 -0500

    fixed naming

--- HW2/models.lua
+++ HW2/models.lua
@@ -23,7 +23,7 @@ end
 
 -- This function builds the neural network used in the paper
 
-function makeNNmodel_figure1(D_0, D_d, D_h)
+function makeNNmodel_figure1(D_0, D_d, D_output)
 
 	local par = nn.ParallelTable() -- takes a TABLE of inputs, applies i'th child to i'th input, and returns a table
 	local sparse_multiply = nn.Sequential()
@@ -37,9 +37,18 @@ function makeNNmodel_figure1(D_0, D_d, D_h)
 	model:add(par)
 	model:add(nn.CAddTable()) -- CAddTable adds its incoming tables
 	model:add(nn.HardTanh())
-	model:add(nn.Linear(100, D_h)) -- second child
+	model:add(nn.Linear(100, D_output)) -- second child
 	model:add(nn.LogSoftMax())
 
 	return model
 
+end
+
+function getaccuracy(model, validation_sparse_input, validation_dense_input, validation_output)
+	scores = model:forward({validation_sparse_input, validation_dense_input})
+	local _, class_preds = torch.max(scores, 2)
+	--print(valueCounts(class_preds:squeeze()))
+	local equality = torch.eq(class_preds, validation_output)
+	local score = equality:sum()/equality:size()[1]
+	return score
 end
