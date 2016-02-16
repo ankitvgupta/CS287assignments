08d238a3e067548c27a586083d0f0bba0cb5f6d0
Ankit Gupta <ankitgupta@college.harvard.edu>
2016-02-16 02:05:35 -0500

    created correct nnet

--- HW2/models.lua
+++ HW2/models.lua
@@ -29,8 +29,17 @@ function makeNNmodel_figure1(D_o, D_d, D_hidden, D_output, loss_function)
 
 	local par = nn.ParallelTable() -- takes a TABLE of inputs, applies i'th child to i'th input, and returns a table
 	local sparse_multiply = nn.Sequential()
-	sparse_multiply:add(nn.LookupTable(D_o, D_hidden))
-	sparse_multiply:add(nn.Sum(1,2))
+	--sparse_multiply:add(nn.LookupTable(D_o, D_hidden))
+	--sparse_multiply:add(nn.Sum(1,2))
+
+	-- I THINK this is now correct.
+	-- Get the word embeddings for each of the words
+	sparse_multiply:add(nn.LookupTable(D_o, 50))
+	-- Flatten those features into a single vector  
+	sparse_multiply:add(nn.View(-1):setNumInputDims(2))
+	-- Apply a linear layer to those.
+	sparse_multiply:add(nn.Linear(50*3, D_hidden))
+
 
 	par:add(sparse_multiply) -- first child
 	par:add(nn.Linear(D_d, D_hidden)) -- second child
@@ -42,6 +51,12 @@ function makeNNmodel_figure1(D_o, D_d, D_hidden, D_output, loss_function)
 	model:add(nn.Linear(D_hidden, D_output)) -- second child
 	model:add(nn.LogSoftMax())
 	criterion = nn.ClassNLLCriterion()
+
+	local x = torch.LongTensor({{45,12,13},{17,3,4}})
+	local x2 = torch.randn(2, D_d)
+	print("forward test")
+	print(par:forward({x, x2}))
+
 
 	return model, criterion
 
