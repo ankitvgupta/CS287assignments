95238f9b4384d38a3dd2a2009483f7b620b5fb66
Ankit Gupta <ankitgupta@college.harvard.edu>
2016-02-22 11:49:41 -0500

    pushing losses

---
+++ HW2/loss_diagram.py
@@ -0,0 +1,29 @@
+# Usage python loss_diagram.py
+import pandas as pd 
+import sys
+import matplotlib.pyplot as plt 
+
+data_lr = pd.read_csv("losses_lr.txt")
+data_nn = pd.read_csv("losses_nn.txt")
+data_nnpre = pd.read_csv("losses_nnpre.txt")
+
+plt.figure()
+plt.title("Logistic Regression Loss")
+plt.plot(data_lr.values[:,0], data_lr.values[:,1])
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+
+plt.figure()
+plt.title("Neural Network Loss")
+plt.plot(data_nn.values[:,0], data_nn.values[:,1])
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+
+plt.figure()
+plt.title("Neural Network (Pretrained Embeddings) Loss")
+plt.plot(data_nnpre.values[:,0], data_nnpre.values[:,1])
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+
+
+plt.show()
