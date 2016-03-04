fd7f6789651f2dabf46a0d21922d7bd0dd121bce
tomsilver <tomssilver@gmail.com>
2016-03-04 15:18:04 -0500

    added output creation

--- HW3/preprocess.py
+++ HW3/preprocess.py
@@ -90,32 +90,54 @@ def create_data_from_blanks(data_file, vocab, dwin):
 
     with open(data_file, 'rb') as f:
         for line in f:
-            line_split = line.split()
             if line[0] == 'Q':
-                options = parse_options(line[1:], vocab)
+                options = parse_options(line[2:], vocab)
                 Os.append(options)
             elif line[0] == 'C':
-                context = parse_context(line[1:], vocab, dwin)
+                context = parse_context(line[2:], vocab, dwin)
                 Cs.append(context)
             else:
                 assert False
 
     return Cs, Os
 
-# TODO
-def create_output_data(data_file):
-    pass
+def create_output_data(valid, valid_blanks, vocab_dict):
+    Y = []
+    with open(valid, 'rb') as vf:
+        with open(valid_blanks, 'rb') as vb:
+            for blanks_line in vb:
+                blank_split = blanks_line.split()
+                if blank_split[0] == 'C':
+                    context = blank_split[1:]
+                    full_text = vf.next()
+                    full_split = full_text.split()
+                    full_split += ['</s>']
+
+                    # if the context is the full sentence, predict another </s>
+                    if context[-1] == '</s>':
+                        full_split.append('</s>')
+
+                    context_len = len(context)
+                    # debugging
+                    assert context_len < len(full_split)
+                    for i, b in enumerate(context):
+                        assert (full_split[i] == b) or (full_split[i] == '<unk>'), full_split
+
+                    target = full_split[context_len]
+                    y = vocab_dict[target]
+                    Y.append(y)
+
+    return Y
+
 
 FILE_PATHS = {"PTB": ("data/train.txt",
                       "data/valid.txt",
                       "data/valid_blanks.txt",
-                      "data/valid_outs.txt",
                       "data/test_blanks.txt",
                       "data/words.dict"),
               "SMALL": ("data/train.1000.txt",
                        "data/valid.1000.txt",
                        "data/valid_blanks.txt",
-                       "data/valid_outs.txt",
                        "data/test_blanks.txt",
                        "data/words.1000.dict")}
 args = {}
@@ -130,13 +152,14 @@ def main(arguments):
                         type=str)
     parser.add_argument('dwin', help="Window size",
                         type=int)
+    parser.add_argument('allwins', help="If true, generate all window sizes <= dwin",
+                        type=bool, default=True, action='store', nargs='?')
+
     args = parser.parse_args(arguments)
     dataset = args.dataset
     dwin = args.dwin
-    train, valid, valid_blanks, valid_outs, test_blanks, word_dict = FILE_PATHS[dataset]
-
-    # Force window size odd
-    assert dwin > 0 and dwin % 2 == 1
+    allwins = args.allwins
+    train, valid, valid_blanks, test_blanks, word_dict = FILE_PATHS[dataset]
 
     # Load vocab dict
     print "Loading vocab dict..."
@@ -145,36 +168,42 @@ def main(arguments):
     numFeatures = len(vocab_dict)
     numClasses = len(vocab_dict)
 
-    print "Creating language samples from training data..."
-    train_input, train_output = create_training_data(train, vocab_dict, dwin)
+    print "Creating validation output..."
+    valid_true_outs = create_output_data(valid, valid_blanks, vocab_dict)
 
-    print "Creating language samples from valid data..."
-    valid_context, valid_options = create_data_from_blanks(valid_blanks, vocab_dict, dwin)
+    if allwins:
+        dwins = range(0, dwin+1)
+    else:
+        dwins = [dwin]
 
-    print "Creating language samples from test data..."
-    test_context, test_options = create_data_from_blanks(test_blanks, vocab_dict, dwin)
+    for dw in dwins:
 
-    # TODO ONCE FILE IS ADDED
-    # print "Creating validation output..."
-    # valid_distribution = create_output_data(valid_output)
+        print "Creating language samples from training data..."
+        train_input, train_output = create_training_data(train, vocab_dict, dw)
 
-    # Write out to hdf5
-    print "Writing out to hdf5"
-    filename = args.dataset + '_'+str(dwin)+'.hdf5'
-    with h5py.File(filename, "w") as f:
-        f['train_input'] = train_input
-        f['train_output'] = train_output
-        if valid_blanks:
-            f['valid_context'] = valid_context
-            f['valid_options'] = valid_options
-            # f['valid_distribution'] = valid_distribution
-        if test_blanks:
-            f['test_context'] = test_context
-            f['test_options'] = test_options
+        print "Creating language samples from valid data..."
+        valid_context, valid_options = create_data_from_blanks(valid_blanks, vocab_dict, dw)
 
-        f['numFeatures'] = np.array([numFeatures], dtype=np.int32)
-        f['numClasses'] = np.array([numClasses], dtype=np.int32)
-        f['d_win'] = np.array([dwin], dtype=np.int32)
+        print "Creating language samples from test data..."
+        test_context, test_options = create_data_from_blanks(test_blanks, vocab_dict, dw)
+
+        # Write out to hdf5
+        filename = args.dataset + '_'+str(dw)+'.hdf5'
+        print "Writing out to "+filename
+        with h5py.File(filename, "w") as f:
+            f['train_input'] = train_input
+            f['train_output'] = train_output
+            if valid_blanks:
+                f['valid_context'] = valid_context
+                f['valid_options'] = valid_options
+                f['valid_true_outs'] = valid_true_outs
+            if test_blanks:
+                f['test_context'] = test_context
+                f['test_options'] = test_options
+
+            f['numFeatures'] = np.array([numFeatures], dtype=np.int32)
+            f['numClasses'] = np.array([numClasses], dtype=np.int32)
+            f['d_win'] = np.array([dw], dtype=np.int32)
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
