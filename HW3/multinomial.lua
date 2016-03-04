fb4086d6f3c5491d0b56db39a8cd937564abba0d
Ankit Gupta <ankitgupta@college.harvard.edu>
2016-03-04 17:32:54 -0500

    updated multinomial

--- HW3/multinomial.lua
+++ HW3/multinomial.lua
@@ -2,6 +2,21 @@ function init_trie()
 	return {}
 end
 
+
+function add_target_to_position(position, target)
+	if position['counts'] == nil then
+		local counts = {}
+		counts[target] = 1
+		position['counts'] = counts
+	-- If we have gotten here, but the target is different
+	elseif position['counts'][target] == nil then
+		position['counts'][target] = 1
+	-- If we have gotten here and seen this target before
+	else
+		position['counts'][target] = position['counts'][target]  + 1
+	end 
+end
+
 -- trie is the trie we are adding to (note that this is passed by reference)
 -- sentence is a torch tensor consisting of the words
 -- The target is optional - if included, then whole window used as context. Otherwise, 
@@ -18,6 +33,8 @@ function add_word_and_context_to_trie(trie, context, target)
 	--local target = context[sentence_len]
 
 	local position = trie
+
+	add_target_to_position(position, target)
 	-- iterate backwards through the context (since this is a reverse trie)
 	for i = sentence_len, 1, -1 do
 		local word = context[i]
@@ -28,19 +45,7 @@ function add_word_and_context_to_trie(trie, context, target)
 			position[word] = {}
 			position = position[word]
 		end
-
-		-- If this is the first time we have gotten here.
-		if position['counts'] == nil then
-			local counts = {}
-			counts[target] = 1
-			position['counts'] = counts
-		-- If we have gotten here, but the target is different
-		elseif position['counts'][target] == nil then
-			position['counts'][target] = 1
-		-- If we have gotten here and seen this target before
-		else
-			position['counts'][target] = position['counts'][target]  + 1
-		end 
+		add_target_to_position(position, target)
 	end
 
 end
@@ -65,15 +70,16 @@ function get_word_counts_for_context(trie, context)
 		end
 
 	end
-	--print(position)
 	return position['counts']
 end
 
+-- Calculates the number of items in a table
 -- This can be used to calculate N_{c,*}
 function number_of_items_in_table(tab)
 	return #tab
 end
 
+-- Sums of the values in a table
 -- This can be used to calculate F_{c,*}
 function sum_of_values_in_table(tab)
 	local total = 0
@@ -83,6 +89,7 @@ function sum_of_values_in_table(tab)
 	return total
 end
 
+-- Multiply each value in a table by x
 function multiply_table_by_x(tab, x)
 	local new_table = {}
 	for key, val in pairs(tab) do
@@ -91,7 +98,9 @@ function multiply_table_by_x(tab, x)
 	return new_table
 end
 
-function sum_tables(tab1, tab2):
+-- Takes the elementwise sum of two tables
+-- This is essentially an outer join, where we sum values on duplicate keys.
+function sum_tables(tab1, tab2)
 	new_table = {}
 	for key,val in pairs(tab1) do
 		new_table[key] = val
@@ -110,7 +119,7 @@ end
 -- input_contexts: Torch LongTensor (N x d_win)
 -- output_words: Torch LongTensor (N x 1)
 -- Both of these should use the IDs of the 
-function train(input_contexts, output_words)
+function fit(input_contexts, output_words)
 
 	-- Make sure the inputs are valid 
 	assert(input_contexts:size(1) == output_words:size(1))
@@ -124,10 +133,6 @@ function train(input_contexts, output_words)
 	return reverse_trie
 end
 
--- function p_ML(count_table)
--- 	return normalize_table(count_table)
--- end
-
 -- Returns the distribution over the vocabulary given the context
 -- Trie should be a trained trie 
 -- Context is a LongTensor.
@@ -136,31 +141,36 @@ end
 function predict(trie, context)
 	local num_words = context:size(1)
 	if num_words == 0 then
-		return {}
+		return nil
 	end
+	-- if num_words == 0 then
+	-- 	return {}
+	-- end
 	local count_table = get_word_counts_for_context(trie, context)
 	local F_cstar = sum_of_values_in_table(count_table)
 	local N_cstar = number_of_items_in_table(count_table) 
 
 	-- This implements F_{c,w} + N_{C,star}*p_wb(w|c')
-	local numerator = sum_tables(count_table, multiply_table_by_x(predict(trie, context:narrow(1, 2, num_words - 1)),N_cstar))
+	local numerator = nil
+	if num_words == 1 then -- base case
+		numerator = sum_tables(count_table, multiply_table_by_x({},N_cstar))
+	else
+		numerator = sum_tables(count_table, multiply_table_by_x(predict(trie, context:narrow(1, 2, num_words - 1)),N_cstar))
+	end
 
 	-- This implements the rest of the fraction
 	local p_wb = multiply_table_by_x(numerator, 1.0/(F_cstar + N_cstar))
 	return p_wb
 end
 
+-- This function might not actually have any value, but
+--      it basically just normalizes the counts in a table.
 function normalize_table(tab)
 	local total = sum_of_values_in_table(tab)
 	return multiply_table_by_x(tab, 1/total)
-	-- local new_table = {}
-	-- for key, val in pairs(tab) do
-	-- 	new_table[key] = val/total
-	-- end
-	-- return new_table
 end
 
-
+-- Creates a simple trie that demonstrates its main features.
 function trie_example()
 	local reverse_trie = init_trie()
 	add_word_and_context_to_trie(reverse_trie, torch.LongTensor{1,2,3},1)
@@ -177,7 +187,10 @@ function trie_example()
 	print(normalize_table(counts))
 end
 
--- Creates a trie with large number of 
+-- Creates a trie with randomly generated words
+--      num_sentences: the number of sentences to insert into the trie
+--      length: The length of each sentence (this assumes fixed length)
+--      vocab_size: The number of words in the vocabulary
 function bigtrie_example(num_sentences, length, vocab_size)
 	local reverse_trie = init_trie()
 	for i = 1, num_sentences do
@@ -192,19 +205,6 @@ function bigtrie_example(num_sentences, length, vocab_size)
     print(get_word_counts_for_context(reverse_trie, torch.LongTensor{1}))
     print(get_word_counts_for_context(reverse_trie, torch.LongTensor{2}))
     print(get_word_counts_for_context(reverse_trie, torch.LongTensor{3}))
-
-    --print(reverse_trie[1]['counts'])
-    --while counts == nil do
-    --	try = try + 1
-    --	print(try)
-	--	counts = get_word_counts_for_context(reverse_trie, torch.LongTensor{try})
-    --end
-    --print(counts)
-    
---	print(reverse_trie)
---	local counts = get_word_counts_for_context(reverse_trie, torch.LongTensor{1,1})
---	print(counts)
---	print(normalize_table(counts))
 end
 
 
