12228c048ed97762eb61590f5a80e1735e1c0d71
tomsilver <tomssilver@gmail.com>
2016-03-06 22:54:01 -0500

    nce draft

--- HW3/models.lua
+++ HW3/models.lua
@@ -21,3 +21,65 @@ function neuralNetwork(D_sparse_in, D_hidden, D_output, embedding_size, window_s
 	return model, criterion
 
 end
+
+--sample_indices a tensor of dimension 1xK
+--sample_probs is the same
+function forwardPass(model, lookuptable, lookuptableparameters, lookuptablegradparameters, input_minibatch, output_minibatch, sample_indices, sample_probs)
+	--dimension of z is minibatch_size x hidden_layer size
+	local z = model:forward(input_minibatch)
+	local K = sample_indices:size(1)
+	local minibatch_size, hidden_size = z:size(1), z:size(2)
+	--print(lookuptable.weight)
+
+	for sample_i =1, sample_indices:size(1) do
+		lookuptablegradparameters:zero()
+		local idx = sample_indices[sample_i]
+		local prob = sample_probs[sample_i]
+
+		local lookuptable_row = lookuptable:forward(torch.LongTensor{idx}):squeeze()
+
+		-- minibatchsize x 1
+		local dot_product = torch.mv(z, lookuptable_row)
+		local subtracted = dot_product - torch.log(K*prob)
+		local sigmoid = nn.Sigmoid()
+		local predictions = sigmoid:forward(subtracted)
+
+		criterion = nn.BCECriterion()
+		local loss = criterion:forward(predictions, torch.zeros(predictions:size(1)))
+
+		dLdpreds = criterion:backward(predictions, torch.zeros(predictions:size(1))) -- gradients of loss wrt preds
+		sigmoid:backward(subtracted, dLdpreds)
+
+		local resizedGrad = (sigmoid.gradInput):view(minibatch_size, 1)
+		local lookup_grad = torch.cmul(z, resizedGrad:expand(minibatch_size, hidden_size))
+
+		lookuptable:backward(torch.LongTensor{idx}, lookup_grad:sum(1))
+		lookuptableparameters:add(torch.mul(lookuptablegradparameters,-.1))
+	end
+
+end
+
+
+function NCE(D_sparse_in, D_hidden, D_output, embedding_size, window_size)
+	print("Making NCE neural network model")
+
+	local model = nn.Sequential()
+	model:add(nn.LookupTable(D_sparse_in, embedding_size))
+	model:add(nn.View(-1):setNumInputDims(2))
+	model:add(nn.Linear(embedding_size*window_size, D_hidden))
+	model:add(nn.Tanh())
+	-- we have z_w and z_{s_i,k} now
+
+
+	return model, nn.LookupTable(D_sparse_in, D_hidden)
+
+end
+
+model, lookup = NCE(10, 2, 4, 2, 3)
+input_batch = torch.LongTensor{{7, 5, 2},{1, 3, 4}}
+output_batch = torch.Tensor{}
+sample_indices = torch.LongTensor{1, 1, 1}
+sample_probs = torch.Tensor{.1, .2, .3}
+params, grads = lookup:getParameters()
+forwardPass(model, lookup, params, grads, input_batch, output_batch, sample_indices, sample_probs)
+
