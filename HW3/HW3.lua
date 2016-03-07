1a35231f2021ceb39944af668b3a538886035f67
Ankit Gupta <ankitgupta@college.harvard.edu>
2016-03-07 18:51:48 -0500

    file path

--- HW3/HW3.lua
+++ HW3/HW3.lua
@@ -41,7 +41,7 @@ function main()
    printoptions(opt)
 
    local f = hdf5.open(opt.datafile, 'r')
-   local f2 = hdf5.open("samples.hdf5")
+   local f2 = hdf5.open(_G.path..'samples.hdf5')
    local samples = f2:read("samples"):all():long()
 
    local nclasses = f:read('numClasses'):all():long()[1]
