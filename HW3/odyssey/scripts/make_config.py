87695b9722761e1b66b2aa5bda25f9b14000c81b
Ankit Gupta <ankitgupta@college.harvard.edu>
2016-03-08 17:35:22 -0500

    added run files

--- HW3/odyssey/scripts/make_config.py
+++ HW3/odyssey/scripts/make_config.py
@@ -1,17 +1,17 @@
 # Use this to quickly make configurations for experiments
 import itertools
 print_description = True
-datafiles=["SMALL_1.hdf5", "SMALL_2.hdf5", "SMALL_3.hdf5", "SMALL_4.hdf5", "SMALL_5.hdf5", "PTB_1.hdf5", "PTB_2.hdf5", "PTB_3.hdf5", "PTB_4.hdf5", "PTB_5.hdf5"]
-classifier=["multinomial"]
-eta=[1]
-minibatchsize=[1]
-numepochs=[20]
-alpha=[.001, .0001, .01, .1, 1, 2, 4, 10]
+datafiles=["PTB_1.hdf5", "PTB_2.hdf5", "PTB_3.hdf5", "PTB_4.hdf5", "PTB_5.hdf5"]
+classifier=["nce"]
+eta=[1, 2, .1, .5, 10, 50, .01]
+minibatchsize=[128]
+numepochs=[20, 40]
+alpha=[.0001, 1]
 lambdas=[1]
 optimizer=["sgd"]
-hiddenlayers=[50]
+hiddenlayers=[50, 100]
 embeddingsize=[50]
-K=[5]
+K=[5,20,40]
 
 lists = [datafiles,classifier,eta,minibatchsize,numepochs,alpha,lambdas,optimizer,hiddenlayers,embeddingsize,K]
 if not print_description:
