b5d5853eaa3a234120fe63f22404eac441c4aee3
Ankit Gupta <ankitgupta@college.harvard.edu>
2016-03-07 22:05:22 -0500

    added run files and updated generator

--- HW3/odyssey/scripts/make_config.py
+++ HW3/odyssey/scripts/make_config.py
@@ -1,9 +1,9 @@
 # Use this to quickly make configurations for experiments
 import itertools
-print_description = False
+print_description = True
 datafiles=["SMALL_1.hdf5", "SMALL_2.hdf5", "SMALL_5.hdf5", "PTB_1.hdf5", "PTB_2.hdf5", "PTB_5.hdf5"]
 classifier=["nce"]
-eta=[.001,.01, .0001, .1, 1]
+eta=[.001,.01, .0001, .1, 1, 10, 100]
 minibatchsize=[256,512,1024]
 numepochs=[20]
 alpha=[.001]
