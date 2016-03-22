b95d611a3fe9d2453bf64308875ef09558867a32
tomsilver <tomssilver@gmail.com>
2016-03-22 14:26:03 -0400

    halfway done with preprocessing

--- HW4/preprocess.py
+++ HW4/preprocess.py
@@ -9,17 +9,32 @@ import argparse
 import sys
 import re
 import codecs
+import string
 
 # Your preprocessing, features construction, and word2vec code.
 
+legal_chars = list(string.printable)+['<space>', '</s>', '</s>\n']
+VOCAB = dict([(v, k+1) for k, v in enumerate(legal_chars)])
 
-
-
-FILE_PATHS = {"PTB": ("data/train.tags.txt",
-                      "data/dev.tags.txt",
-                      "data/test.tags.txt",
-                      "data/tags.dict")}
+FILE_PATHS = {"PTB": ("data/train_chars.txt",
+                      "data/valid_chars.txt",
+                      "data/valid_chars_kaggle.txt",
+                      "data/test_chars.txt"
+                      )}
 args = {}
+
+def file_to_input(file_path):
+    space_index = VOCAB['<space>']
+    with open(file_path, 'r') as f:
+        full_sample = f.next()
+        all_chars = full_sample.split(' ')
+        X = np.array([VOCAB[c] for c in all_chars])
+        spaces = np.argwhere(X==space_index)
+        next_spaces = spaces-1
+        Y = np.zeros(X.shape)
+        Y[next_spaces] = 1
+        return X, Y
+
 
 
 def main(arguments):
@@ -28,22 +43,24 @@ def main(arguments):
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('dataset', help="Data set",
-                        type=str)
+                        type=str, default='PTB', nargs='?')
     args = parser.parse_args(arguments)
     dataset = args.dataset
-    train, valid, test, tag_dict = FILE_PATHS[dataset]
+    train, valid, valid_kaggle, test = FILE_PATHS[dataset]
 
-    filename = args.dataset + '.hdf5'
-    with h5py.File(filename, "w") as f:
-        f['train_input'] = train_input
-        f['train_output'] = train_output
-        if valid:
-            f['valid_input'] = valid_input
-            f['valid_output'] = valid_output
-        if test:
-            f['test_input'] = test_input
-        f['nfeatures'] = np.array([V], dtype=np.int32)
-        f['nclasses'] = np.array([C], dtype=np.int32)
+    train_input, train_output = file_to_input(train)
+
+    # filename = args.dataset + '.hdf5'
+    # with h5py.File(filename, "w") as f:
+    #     f['train_input'] = train_input
+    #     f['train_output'] = train_output
+    #     if valid:
+    #         f['valid_input'] = valid_input
+    #         f['valid_output'] = valid_output
+    #     if test:
+    #         f['test_input'] = test_input
+    #     f['nfeatures'] = np.array([V], dtype=np.int32)
+    #     f['nclasses'] = np.array([C], dtype=np.int32)
 
 
 if __name__ == '__main__':
