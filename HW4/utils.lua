91601ddc1cc5ade6e339755d9ea8a31f4af46f00
Ankit Gupta <ankitgupta@college.harvard.edu>
2016-03-29 14:35:20 -0400

    added more print options

--- HW4/utils.lua
+++ HW4/utils.lua
@@ -102,7 +102,20 @@ function find_matching(tensor, num)
 end
 
 function printoptions(opt)
-	print("datafile:", opt.datafile, "classifier:", opt.classifier, "window_size:", opt.window_size, "b:", opt.b, "alpha:", opt.alpha, "sequence_length:", opt.sequence_length, "embedding_size:", opt.embedding_size, "optimizer:", opt.optimizer, "epochs:", opt.epochs, "hidden", opt.hidden, "eta:", opt.eta, "hacks", opt.hacks_wanted)
+	print("datafile:", opt.datafile, 
+		"classifier:", opt.classifier, 
+		"window_size:", opt.window_size, 
+		"b:", opt.b, "alpha:", opt.alpha, 
+		"sequence_length:", opt.sequence_length, 
+		"embedding_size:", opt.embedding_size, 
+		"optimizer:", opt.optimizer, 
+		"epochs:", opt.epochs, 
+		"hidden", opt.hidden, 
+		"eta:", opt.eta, 
+		"hacks", opt.hacks_wanted,
+		"rnn1", opt.rnn_unit1,
+		"rnn2", opt.rnn_unit2,
+		"dropout", opt.dropout)
 end
 
 function get_result_accuracy(result, validation_input, validation_options, validation_true_outs)
