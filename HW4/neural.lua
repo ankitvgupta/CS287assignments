1f50929f5b0ea186e95f41bc6b09e0ca72868641
tomsilver <tomssilver@gmail.com>
2016-04-02 17:27:13 -0400

    nn viterbi

--- HW4/neural.lua
+++ HW4/neural.lua
@@ -240,6 +240,80 @@ function nn_greedily_segment(flat_valid_input, model, window_size, space_idx)
 
 end
 
+function nn_viterbi_segment(flat_valid_input, model, window_size, space_idx)
+
+	print("Starting viterbi")
+	local valid_input_count = flat_valid_input:size(1)
+	local backpointers = torch.ones(valid_input_count, 2):long()
+
+	-- This is the probabilities if the last one was a space.
+	local post_space_probs = model:forward(torch.Tensor{space_idx})
+
+	-- BIGRAMS ONLY
+	local next_window = torch.Tensor(1)
+	next_window[1] = flat_valid_input[1]
+
+	local pi = torch.ones(valid_input_count, 2):mul(-1e+31)
+	pi[1] = model:forward(next_window)
+	--pi[1][1] = 0
+	--pi[1][2] = 0
+
+	for i = 2, valid_input_count do
+
+		next_window[1] = flat_valid_input[i]
+		local yci11 = model:forward(next_window)
+			
+		-- Last one was not a space, and next is not a space.
+		local score1 = pi[i-1][1] + yci11[1]
+
+		-- Last one was not a space, and next is a space.
+		local score3 = pi[i-1][1] + yci11[2]
+
+		-- Last one was a space, and next is not a space.
+		local score2 = pi[i-1][2] + yci11[1]
+
+		-- Last one was a space, and next one is a space.
+		local score4 = pi[i-1][2] + yci11[2]
+
+		-- The argmax thing.
+		if score1 > pi[i][1] then
+			pi[i][1] = score1
+			backpointers[i][1] = 1
+		end
+		if score2 > pi[i][1] then
+			pi[i][1] = score2
+			backpointers[i][1] = 2
+		end
+		if score3 > pi[i][2] then
+			pi[i][2] = score3
+			backpointers[i][2] = 1
+		end
+		if score4 > pi[i][2] then
+			pi[i][2] = score4
+			backpointers[i][2] = 2
+		end
+
+	end
+
+	print(backpointers:narrow(1, 1, 20))
+
+	local valid_output_predictions = torch.ones(valid_input_count):long()
+
+	local last_class = 1
+	if pi[valid_input_count][2] > pi[valid_input_count][1] then
+		last_class = 2
+	end
+
+	valid_output_predictions[valid_input_count] = last_class
+
+	for i=valid_input_count-1, 1, -1 do
+		valid_output_predictions[i] = backpointers[i+1][valid_output_predictions[i+1]]
+	end
+
+	return valid_output_predictions
+
+end
+
 function rnn_greedily_segment(flat_valid_input, model, space_idx, padding_idx)
 
 	--print("Starting predictions")
