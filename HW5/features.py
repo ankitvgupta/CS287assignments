3027dced3d6a74cc1d6c164e33bd21cc8a3df5c9
tomsilver <tomssilver@gmail.com>
2016-04-05 23:20:51 -0400

    preprocess skeleton done

---
+++ HW5/features.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+"""Classes for binary features of words for NER
+"""
+
+class Feature(object):
+
+	def __init__(self, dwin):
+		self.dwin = dwin
+
+	def initialize(self):
+		pass
+
+	def convert(self, padded_sentence, word_idx):
+		raise UnimplementedError
+
+	def isSparse(self):
+		raise UnimplementedError
+
+	def validate_sentence(self, padded_sentence, word_idx):
+		l = len(padded_sentence)
+		assert word_idx >= self.dwin/2 and word_idx < l-self.dwin/2
+		for i in range(0, self.dwin/2):
+			assert padded_sentence[i] == "PADDING"
+			assert padded_sentence[l-i-1] == "PADDING"
+
+	def numFeats(self):
+		raise UnimplementedError
+
+
+class UnigramFeature(Feature):
+	def __init__(self, dwin, vocab):
+		self.dwin = dwin
+		self.vocab = vocab
+		self.word_to_idx = {}
+
+	def initialize(self):
+		for i, word in enumerate(self.vocab):
+			self.word_to_idx[word] = i+1
+
+		self.numWords = len(self.vocab)
+
+	def convert(self, padded_sentence, word_idx):
+		self.validate_sentence(padded_sentence, word_idx)
+
+		feat = []
+		start = word_idx - self.dwin/2
+		end = word_idx + self.dwin/2
+		for idx, i in enumerate(range(start, end+1)):
+			word = padded_sentence[i]
+			if word is not "PADDING":
+				word = word.lower()
+			try:
+				feat.append(self.word_to_idx[word])
+			except KeyError:
+				feat.append(self.word_to_idx["RARE"])
+
+		return feat
+
+	def isSparse(self):
+		return True
+
+	def numFeats(self):
+		return self.numWords
+
+
+class CapitalizationFeature(Feature):
+
+	# for each word,
+	# first entry is all lowercase
+	# second entry is all caps
+	# third entry is first letter capital
+	# fourth entry is one letter capital
+	def convert(self, padded_sentence, word_idx):
+		self.validate_sentence(padded_sentence, word_idx)
+
+		feat = []
+		start = word_idx - self.dwin/2
+		end = word_idx + self.dwin/2
+		for i in range(start, end+1):
+			word = padded_sentence[i]
+			all_lower = 1+int(word.islower())
+			all_caps = 1+int(word.isupper())
+			first_cap = 1+int(word[0].isupper())
+			one_letter_cap = 1+int(len([c for c in word if c.isupper()]) == 1)
+			feat.extend([all_lower, all_caps, first_cap, one_letter_cap])
+
+		return feat
+
+	def isSparse(self):
+		return False
+
+	def numFeats(self):
+		return 4*self.dwin
--- HW5/preprocess.py
+++ HW5/preprocess.py
@@ -1,16 +1,158 @@
 #!/usr/bin/env python
 
 """NER Preprocessing
+Feature converters take in padded sentence, index, and window size
+
+0. Load tag dict
+1. Go through dataset once
+    -Create vocab with top 100000 (lowercase) words
+    -Initialize features (unigram feature)
+        -Unigram feature should take sentence, output sparse indices (handle RARE here)
+        -Capitalization feature does not need to see the dataset
+2. Go through dataset again
+    -For each sentence, add padding and handle NUMBER
+        -For each word, apply sparse and dense feature converters
+        -Add NER index to Y
+    -Output X_sparse, X_dense, Y
+3. Save to hdf5:
+    -'train_input_sparse'
+    -'train_input_dense'
+    -'train_output'
+    -'valid_input_sparse'
+    -'valid_input_dense'
+    -'valid_output'
+    -'test_input_sparse'
+    -'test_input_dense'
 """
+from collections import defaultdict
+from features import CapitalizationFeature, UnigramFeature
 
 import numpy as np
 import h5py
 import argparse
 import sys
+import random
 import re
 import codecs
 
-# Your preprocessing, features construction, and word2vec code.
+
+def load_tag_dict(file_path):
+    tag_dict = {}
+    with open(file_path, 'r') as f:
+        for line in f:
+            tag, idx = line.strip().split(' ')
+            tag_dict[tag] = int(idx)
+
+    return tag_dict
+
+def parse_line(s, num_sub="NUMBER", lowercase=False, sep='\t'):
+    assert s[0].isdigit()
+    s = s.strip()
+    try:
+        global_id, sentence_id, word, tag = s.split(sep)
+    # test file has no tag
+    except ValueError:
+        tag = None
+        global_id, sentence_id, word = s.split(sep)
+    if lowercase:
+        word = word.lower()
+    word = re.sub('\d', num_sub, word)
+    return word, tag
+
+
+def init_vocab(file_path, top_n=100000):
+    vocab_dict = defaultdict(int)
+
+    with open(file_path, 'r') as f:
+        for line in f:
+            if line[0].isdigit():
+                word, _ = parse_line(line, lowercase=True)
+                vocab_dict[word] += 1    
+
+    top_n_words = sorted(vocab_dict, key=vocab_dict.get, reverse=True)[:top_n]
+    return top_n_words+['RARE', 'PADDING']
+
+
+def init_features(feature_list):
+    inited_features = []
+    numSparseFeatures = 0
+    numDenseFeatures = 0
+
+    for feature in feature_list:
+        if type(feature) is tuple:
+            kwargs = feature[1]
+            feature = feature[0]
+        else:
+            kwargs = {}
+
+        inited_feature = feature(**kwargs)
+        inited_feature.initialize()
+        inited_features.append(inited_feature)
+
+        if inited_feature.isSparse():
+            numSparseFeatures += inited_feature.numFeats()
+        else:
+            numDenseFeatures += inited_feature.numFeats()
+
+    return inited_features, numSparseFeatures, numDenseFeatures
+
+def load_padded_sentences(data_file, dwin, sep):
+    all_sentences = []
+    this_sentence = ['PADDING' for _ in range(dwin/2)]
+
+    with open(data_file, 'r') as f:
+        for line in f:
+            if line[0].isdigit():
+                word, _ = parse_line(line, sep=sep)
+                this_sentence.append(word)
+            # NEW SENTENCE!
+            else:
+                # finish the old
+                this_sentence = this_sentence + ['PADDING' for _ in range(dwin/2)]
+                all_sentences.append(this_sentence)
+                this_sentence = ['PADDING' for _ in range(dwin/2)]
+
+    return all_sentences
+
+def create_input(data_file, dwin, features, sep='\t'):
+    sparse_features = [f for f in features if f.isSparse()]
+    dense_features = [f for f in features if not f.isSparse()]
+    padded_sentences = load_padded_sentences(data_file, dwin, sep)
+    sparse_X = []
+    dense_X = []
+
+    for sentence in padded_sentences:
+        start = dwin/2
+        end = len(sentence)-dwin/2-1
+        for i in range(start, end+1):
+
+            for sparse_feat in sparse_features:
+                feat = sparse_feat.convert(sentence, i)
+                sparse_X.append(feat)
+
+            for dense_feat in dense_features:
+                feat = dense_feat.convert(sentence, i)
+                dense_X.append(feat)
+
+    return sparse_X, dense_X
+
+
+def create_output(data_file, tag_dict):
+    Y = []
+
+    with open(data_file, 'r') as f:
+        for line in f:
+            if line[0].isdigit():
+                _, tag = parse_line(line)
+                try:
+                    Y.append(tag_dict[tag])
+                except KeyError:
+                    print line
+                    print tag
+                    assert False
+
+    return Y
+
 
 
 FILE_PATHS = {"CONLL": ("data/train.num.txt",
@@ -18,6 +160,8 @@ FILE_PATHS = {"CONLL": ("data/train.num.txt",
                         "data/test.num.txt",
                         "data/tags.txt")}
 args = {}
+
+WORD_EMBEDDINGS_FILE = None
 
 
 def main(arguments):
@@ -29,19 +173,70 @@ def main(arguments):
                         type=str)
     args = parser.parse_args(arguments)
     dataset = args.dataset
-    train, valid, test, tag_dict = FILE_PATHS[dataset]
+    train, valid, test, tag_file = FILE_PATHS[dataset]
 
-    filename = args.dataset + '.hdf5'
+    # hack to continue using old code...
+    dwin = 1
+
+    # Load tag dict
+    print "Loading tag dict..."
+    tag_dict = load_tag_dict(tag_file)
+    # Initialize vocabulary, a list of words
+    if WORD_EMBEDDINGS_FILE is not None:
+        print "Initializing vocabulary (and word embeddings)..."
+        vocab, word_embeddings = init_vocab_and_embeddings(WORD_EMBEDDINGS_FILE, 500000)
+    else:
+        print "Initializing vocabulary..."
+        vocab = init_vocab(train)
+        word_embeddings = None
+    # Initialize features
+    print "Initializing features..."
+    features, numSparseFeatures, numDenseFeatures = init_features([(UnigramFeature, {'vocab': vocab, 'dwin': dwin}), (CapitalizationFeature, {'dwin': dwin})])
+
+    numClasses = len(tag_dict)
+    print "sparse, dense, classes:"
+    print numSparseFeatures, numDenseFeatures, numClasses
+
+    sparse_Xs = []
+    dense_Xs = []
+    Ys = []
+
+    for i, data_file in enumerate([train, valid, test]):
+        # Create sparse and dense inputs
+        print "Creating input from "+data_file
+        if i < 2:
+            sparse_X, dense_X = create_input(data_file, dwin, features)
+        # annoying discrepancy with the test file
+        else:
+            sparse_X, dense_X = create_input(data_file, dwin, features, sep=None)
+        sparse_Xs.append(sparse_X)
+        dense_Xs.append(dense_X)
+
+        if i < 2:
+            # Create output (POS tags)
+            print "Creating output"
+            Y = create_output(data_file, tag_dict)
+            Ys.append(Y)
+
+    # Write out to hdf5
+    print "Writing out to hdf5"
+    filename = args.dataset + '_'+str(dwin)+'.hdf5'
     with h5py.File(filename, "w") as f:
-        f['train_input'] = train_input
-        f['train_output'] = train_output
-        if valid:
-            f['valid_input'] = valid_input
-            f['valid_output'] = valid_output
-        if test:
-            f['test_input'] = test_input
-        f['nfeatures'] = np.array([V], dtype=np.int32)
-        f['nclasses'] = np.array([C], dtype=np.int32)
+        for i, data_name in enumerate(['train', 'valid', 'test']):
+            
+            f[data_name+'_sparse_input'] = sparse_Xs[i]
+            f[data_name+'_dense_input'] = dense_Xs[i]
+            
+            if data_name != 'test':
+                f[data_name+'_output'] = Ys[i]
+
+        if word_embeddings is not None:
+            f['word_embeddings'] = word_embeddings
+        else:
+            f['word_embeddings'] = np.array([0], dtype=np.int32)
+        f['numSparseFeatures'] = np.array([numSparseFeatures], dtype=np.int32)
+        f['numDenseFeatures'] = np.array([numDenseFeatures], dtype=np.int32)
+        f['numClasses'] = np.array([numClasses], dtype=np.int32)
 
 
 if __name__ == '__main__':
