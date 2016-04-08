1e26e41675cfaf25fd54f9644a3a5254410d020c
tomsilver <tomssilver@gmail.com>
2016-04-08 10:44:26 -0400

    moved things around

--- HW5/utils.lua
+++ HW5/utils.lua
@@ -45,28 +45,3 @@ function viterbi(x, predictor, numClasses, start_class, debugger)
 
 	return yhat
 end
-
--- expected output:
--- pi:	 0.0000e+00 -1.0000e+31 -1.0000e+31
---  2.0000e+00  1.0000e+00  0.0000e+00
---  5.0000e+00  4.0000e+00  3.0000e+00
---  1.1000e+01  1.0000e+01  9.0000e+00
--- [torch.DoubleTensor of size 4x3]
-
---  1
---  3
---  3
---  1
-function test_viterbi(viterbi_alg)
-	local x = torch.Tensor{1, 3, 2, 3}:resize(4, 1)
-	local predictor = function (ci1, x) 
-						return torch.Tensor{torch.exp(ci1*x[1]-1),
-											torch.exp(ci1*x[1]-2),
-											torch.exp(ci1*x[1]-3)} end
-	local numClasses = 3
-	local start_class = 1
-	local yhat = viterbi_alg(x, predictor, numClasses, start_class, true)
-	print(yhat)
-end
-
-test_viterbi(viterbi)
