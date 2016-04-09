d7e00dc96c74241b6a944cf50110990e05d82577
tomsilver <tomssilver@gmail.com>
2016-04-08 21:54:48 -0400

    better hmm test

--- HW5/test.lua
+++ HW5/test.lua
@@ -9,32 +9,49 @@ end
 
 x = torch.Tensor{1,2,3}
 
-predictor = make_predictor(x)
-x:add(1)
-print(predictor(1, 2))
+-- predictor = make_predictor(x)
+-- x:add(1)
+-- print(predictor(1, 2))
 
 
--- expected output:
--- pi:	 0.0000e+00 -1.0000e+31 -1.0000e+31
---  2.0000e+00  1.0000e+00  0.0000e+00
---  5.0000e+00  4.0000e+00  3.0000e+00
---  1.1000e+01  1.0000e+01  9.0000e+00
--- [torch.DoubleTensor of size 4x3]
-
---  1
---  3
+-- expect 
 --  3
 --  1
+--  1
+--  1
+--  2
+--  2
+--  2
+--  2
+--  2
+--  2
+-- [torch.DoubleTensor of size 10]
 function test_viterbi(viterbi_alg)
-	local x = torch.Tensor{1, 3, 2, 3}:resize(4, 1)
-	local predictor = function (ci1, x) 
-						return torch.Tensor{torch.exp(ci1*x[1]-1),
-											torch.exp(ci1*x[1]-2),
-											torch.exp(ci1*x[1]-3)} end
+	local x = torch.Tensor{5, 3, 3, 2, 1, 2, 4, 3, 1, 1}:resize(10, 1)
+	local predictor = (function (ci1, x) 
+						if (x[1] == 5) then
+							return torch.Tensor{0, 0, 1.0}
+						elseif (ci1 == 3) then
+							return torch.Tensor{0.5, 0.5, 0}
+						elseif (ci1 == 1) then
+							if ((x[1] == 1) or (x[1] == 4)) then
+								return torch.Tensor{0.1, 0.15, 0}
+							else
+								return torch.Tensor{0.15, 0.1, 0}
+							end
+						else
+							if ((x[1] == 1) or (x[1] == 4)) then
+								return torch.Tensor{0.08, 0.18, 0}
+							else
+								return torch.Tensor{0.12, 0.12, 0}
+							end
+						end
+						end)
+
 	local numClasses = 3
-	local start_class = 1
-	local yhat = viterbi_alg(x, predictor, numClasses, start_class, true)
+	local start_class = 3
+	local yhat = viterbi_alg(x, predictor, numClasses, start_class)
 	print(yhat)
 end
 
---test_viterbi(viterbi)
+test_viterbi(viterbi)
