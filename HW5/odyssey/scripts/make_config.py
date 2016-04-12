0630a72f2bef6e92630ad129ae3c655d95b555de
Ankit Gupta <ankitgupta@college.harvard.edu>
2016-04-12 15:07:37 -0400

    added config for test run

--- HW5/odyssey/scripts/make_config.py
+++ HW5/odyssey/scripts/make_config.py
@@ -1,16 +1,16 @@
 # Use this to quickly make configurations for experiments
 import itertools
-print_description = False
-datafiles=["PTB.hdf5"]
-classifier=["neural"]
+print_description = True
+datafiles=["CONLL_3.hdf5"]
+classifier=["struct"]
 alpha=[1]
-beta=[]
-embedding_size=[15,25,45,55]
+beta=[1]
+embedding_size=[25, 50]
 minibatch_size=[320]
-optimizer=["sgd","adagrad"]
-epochs=[100]
-hidden=[15,25]
-eta=[.001, .01, .1, .0001]
+optimizer=["sgd"]
+epochs=[50]
+hidden=[50]
+eta=[.01, .1, .05, .5]
 
 
 lists = [datafiles,classifier,alpha, beta, embedding_size, minibatch_size, optimizer, epochs, hidden, eta]
---
+++ HW5/odyssey/scripts/run_struct_test1.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#SBATCH -n 1                         #Number of cores
+#SBATCH -N 1                          #Run on 1 node
+#SBATCH --mem=2000                  #Memory per cpu in MB (see also --mem)
+
+#SBATCH -t 5:00:00              #Indicate duration using HH:MM:SS
+#SBATCH -p serial_requeue               #Partition to submit to
+
+#SBATCH -o /n/home09/ankitgupta/CS287/CS287assignments/HW5/odyssey/outputs/out/struct_test1/setup_%A_%a_out.txt            #File to which standard out will be written
+#SBATCH -e /n/home09/ankitgupta/CS287/CS287assignments/HW5/odyssey/outputs/err/struct_test1/setup_%A_%a_err.txt            #File to which standard err will be written
+#SBATCH --mail-type=ALL                 #Type of email notification- BEGIN,END,FAIL,ALL
+#SBATCH --mail-user=ankitgupta@college.harvard.edu  #Email to which notifications will be sent
+
+# Read the config files and get the appropriate config
+readarray -t config < /n/home09/ankitgupta/CS287/CS287assignments/HW5/odyssey/scripts/struct_test_config.txt
+selected_config=(${config[$SLURM_ARRAY_TASK_ID]})
+
+# Extract params
+datafile=${selected_config[0]}
+classifier=${selected_config[1]}
+alpha=${selected_config[2]}
+beta=${selected_config[3]}
+embedding_size=${selected_config[4]}
+minibatch_size=${selected_config[5]}
+optimizer=${selected_config[6]}
+epochs=${selected_config[7]}
+hidden=${selected_config[8]}
+eta=${selected_config[9]}
+
+# Run the trainer
+cd /scratch
+source /n/home09/ankitgupta/torch_setup.sh
+th /n/home09/ankitgupta/CS287/CS287assignments/HW5/HW5.lua \
+  -datafile /n/home09/ankitgupta/CS287/CS287assignments/HW5/$datafile \
+  -classifier $classifier \
+  -alpha $alpha \
+  -beta $beta \
+  -embedding_size $embedding_size \
+  -minibatch_size $minibatch_size \
+  -optimizer $optimizer \
+  -epochs $epochs \
+  -hidden $hidden \
+  -eta $eta \
+  -odyssey
+
+
+
+
+
