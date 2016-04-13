8ed8669768b4d1eecb40291d0045a4729a320d9f
Ankit Gupta <ankitgupta@college.harvard.edu>
2016-04-12 22:38:45 -0400

    added config for memm

--- HW5/odyssey/scripts/make_config.py
+++ HW5/odyssey/scripts/make_config.py
@@ -1,16 +1,16 @@
 # Use this to quickly make configurations for experiments
 import itertools
 print_description = True
-datafiles=["CONLL_3.hdf5"]
-classifier=["struct"]
+datafiles=["CONLL_1.hdf5", "CONLL_3.hdf5"]
+classifier=["memm"]
 alpha=[1]
 beta=[1]
-embedding_size=[25, 50]
-minibatch_size=[320]
-optimizer=["sgd"]
+embedding_size=[15, 25, 50, 75, 100]
+minibatch_size=[64, 128, 256]
+optimizer=["sgd", "adagrad"]
 epochs=[50]
-hidden=[50]
-eta=[.01, .1, .05, .5]
+hidden=[25, 35, 55]
+eta=[.01, .1, 1]
 
 
 lists = [datafiles,classifier,alpha, beta, embedding_size, minibatch_size, optimizer, epochs, hidden, eta]
