6b01d365e2148712a673dedb73a8514a3f8ad608
Ankit Gupta <ankitgupta@college.harvard.edu>
2016-04-15 23:29:20 +0400

    updated scripts

--- HW5/odyssey/scripts/make_config.py
+++ HW5/odyssey/scripts/make_config.py
@@ -1,16 +1,17 @@
 # Use this to quickly make configurations for experiments
 import itertools
 print_description = True
-datafiles=["CONLL_1.hdf5", "CONLL_3.hdf5"]
-classifier=["memm"]
+datafiles=['CONLL_3.hdf5']
+classifier=['struct']
 alpha=[1]
 beta=[1]
-embedding_size=[15, 25, 50, 75, 100]
-minibatch_size=[64, 128, 256]
-optimizer=["sgd", "adagrad"]
-epochs=[50]
-hidden=[25, 35, 55]
-eta=[.01, .1, 1]
+embedding_size=[25, 50, 75, 100]
+minibatch_size=[1]
+optimizer=["sgd"]
+epochs=[10, 30, 50]
+hidden=[0, 25, 35, 55]
+eta=[0.01, 0.005, 0.001, 0.0005, .1]
+
 
 lists = [datafiles,classifier,alpha, beta, embedding_size, minibatch_size, optimizer, epochs, hidden, eta]
 if not print_description:
