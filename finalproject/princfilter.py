1d5efd8b74ba07123d6c255f582331b7f876fdc2
tomsilver <tomssilver@gmail.com>
2016-04-29 19:41:14 -0400

    filtering script

---
+++ finalproject/princfilter.py
@@ -0,0 +1,120 @@
+from Bio import pairwise2
+from Bio.SubsMat import MatrixInfo as matlist
+
+import argparse
+import numpy as np
+import sys
+
+matrix = matlist.blosum62
+gap_open = -10
+gap_extend = -0.5
+
+def parse_human(data_file):
+    X_strings = []
+
+    next_line_is_seq = False
+    next_line_is_output = False
+
+    with open(data_file, 'r') as f:
+        for line in f:
+            if 'A:sequence' in line:
+                next_line_is_output = False
+                next_line_is_seq = True
+            elif 'A:secstr' in line:
+                next_line_is_seq = False
+                next_line_is_output = True
+            elif (':sequence' in line) or ('secstr' in line):
+                next_line_is_seq = False
+                next_line_is_output = False
+            elif next_line_is_seq: 
+                X_strings.append(line[:-1])
+            # elif next_line_is_output:
+            #     Y_strings.append(line[:-1])
+
+    return X_strings
+
+def parse_princeton(data_file, num_proteins):
+    X_strings = []
+
+    # NoSeq is padding
+    acid_order = ['A', 'C', 'E', 'D', 'G', 'F', 'I', 'H', 'K', 'M', 'L', 'N', 'Q', 'P', 'S', 'R', 'T', 'W', 'V', 'Y', 'X','NoSeq']
+
+    data = np.load(data_file)
+    amino_acids = np.reshape(data, (num_proteins, 700, 57))
+    
+    for p in range(num_proteins):
+        x = ''
+        for a in range(700):
+            # reached end of this sequence
+            if amino_acids[p][a][21]:
+                # label should also be noseq
+                assert amino_acids[p][a][30]
+                X_strings.append(x)
+                break
+            else:
+                this_acids = np.nonzero(amino_acids[p][a][:22])[0]
+                assert len(this_acids) == 1
+                this_acid = acid_order[this_acids[0]]
+
+                x = x+this_acid
+
+
+    return X_strings
+
+def sim(aln_seq1,aln_seq2):
+    c=0.0
+    for i,j in zip(aln_seq1,aln_seq2):
+        if i ==j:
+            c=c+1
+    return c/min(len(aln_seq1),len(aln_seq2))
+
+def identity_score(seq1, seq2):
+	top_aln = pairwise2.align.globalds(seq1,seq2, matrix, gap_open, gap_extend, one_alignment_only=1)[0]
+	aln_seq1, aln_seq2, score, begin, end = top_aln
+	return sim(aln_seq1,aln_seq2)
+
+def mock_score(seq1, seq2):
+	return 0.0
+
+def pfilter(sequences, filter_out, start_idx, l, identity_thresh=0.25, identity_map=identity_score, verbose=True):
+	seq_idxs = []
+	for idx in range(start_idx, start_idx+l):
+		# if idx % 1 == 0:
+		# 	print "Filtering sequence ", idx+1, "... (",
+		seq1 = sequences[idx]
+		keep = True
+		for seq2 in filter_out:
+			if identity_map(seq1, seq2) >= identity_thresh:
+				keep = False
+				break
+		if keep:
+			seq_idxs.append(idx)
+			if verbose:
+				print idx
+	return seq_idxs
+
+def main(arguments):
+	global args
+	parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+	parser.add_argument('cb513', help="CB513 path", type=str)
+	parser.add_argument('train', help="Training set path", type=str)
+	parser.add_argument('start', help="Start index", type=int)
+	parser.add_argument('count', help="Number of indices", type=int)
+
+	args = parser.parse_args(arguments)
+	cb513_path = args.cb513
+	train_path = args.train
+	start_idx = args.start
+	count = args.count
+
+	train_seqs = parse_human(train_path)
+	cb513_seqs = parse_princeton(cb513_path, 514)
+
+	pfilter(train_seqs, cb513_seqs, start_idx, count)
+
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
