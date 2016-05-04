7c7ed3de205958158c2745ff6b226a22666fc320
Ankit Gupta <ankitgupta@college.harvard.edu>
2016-05-04 16:02:55 -0400

    committing testing code

--- finalproject/testing.lua
+++ finalproject/testing.lua
@@ -4,25 +4,35 @@ require 'rnn'
 
 batchLSTM = nn.Sequential()
 vocab_size = 20
-embed_dim = 30
+embed_dim = 8
 output_dim = 5
-local inputs = torch.randn(4, 6):abs():mul(4):long() + 1
+dwin = 3
+local inputs = torch.randn(4, 6, dwin):abs():mul(4):long() + 1
 print(inputs)
-
-local embedding = nn.LookupTable(vocab_size, embed_dim)
-batchLSTM:add(embedding) --will return a sequence-length x batch-size x embedDim tensor
-batchLSTM:add(nn.Transpose({1,2}))
 batchLSTM:add(nn.SplitTable(1, 3))
 
---batchLSTM:add(nn.Sequencer(nn.FastLSTM(embed_dim, embed_dim)))
-biseq = nn.BiSequencer(nn.FastLSTM(embed_dim, embed_dim), nn.FastLSTM(embed_dim, embed_dim), 1)
-batchLSTM:add(biseq)
 print(batchLSTM:forward(inputs))
-print("Test")
-print(nn.JoinTable(1,1):forward({torch.randn(4, 30), torch.randn(4, 30)}))
-print(biseq.bwdSeq)
+local embedding = nn.Sequencer(nn.LookupTable(vocab_size, embed_dim))
+--local embedding = nn.TemporalConvolution(3, embed_dim, 1, 1)
+batchLSTM:add(embedding) --will return a sequence-length x batch-size x embedDim tensor
+batchLSTM:add(nn.Sequencer(nn.View(-1):setNumInputDims(2)))
+batchLSTM:add(nn.JoinTable(1, 3))
+batchLSTM:add(nn.View(4, 6, dwin*embed_dim))
+--print(batchLSTM:forward(inputs))
+batchLSTM:add(nn.Transpose({1,2}))
+batchLSTM:add(nn.SplitTable(1, 3))
+print(batchLSTM:forward(inputs))
 
-batchLSTM:add(nn.Sequencer(nn.Linear(2*embed_dim, output_dim)))
+--batchLSTM:add(nn.Sequencer(nn.FastLSTM(embed_dim, embed_dim)))
+biseq = nn.BiSequencer(nn.FastLSTM(dwin*embed_dim, dwin*embed_dim), nn.FastLSTM(dwin*embed_dim, dwin*embed_dim), 1)
+batchLSTM:add(biseq)
+--print(batchLSTM:forward(inputs))
+--print("Test")
+--print(nn.JoinTable(1,1):forward({torch.randn(4, 30), torch.randn(4, 30)}))
+--print(biseq.bwdSeq)
+
+batchLSTM:add(nn.Sequencer(nn.Linear(2*dwin*embed_dim, output_dim)))
 batchLSTM:add(nn.Sequencer(nn.LogSoftMax()))
 print(batchLSTM:forward(inputs))
+print(batchLSTM:forward(inputs)[1])
 
