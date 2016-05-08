e6d64c7b203c2dfa828538a36511535c388ae3e6
Ankit Gupta <ankitgupta@college.harvard.edu>
2016-05-08 18:03:04 -0400

    updating everything

--- finalproject/odyssey/scripts/make_config.py
+++ finalproject/odyssey/scripts/make_config.py
@@ -1,19 +1,19 @@
 # Use this to quickly make configurations for experiments
 import itertools
-print_description = True
+print_description = False
 datafiles=["EPRINC_CB513_1.hdf5"]
 classifier=["rnn"]
 b=[128]
 alpha=[1]
-sequence_length=[50, 100]
+sequence_length=[100]
 embedding_size=[50, 100]
 optimizer=["sgd"]
 epochs=[200]
 hidden=[100]
-eta=[.1, .05]
+eta=[.1, .05, .01]
 rnn1=["lstm"]
 rnn2=["lstm"]
-dropout=[.5, .25, .75] 
+dropout=[.15, .25, .1, .05] 
 layers=[2,3]
 
 lists = [datafiles,classifier, b, alpha, sequence_length, embedding_size, optimizer, epochs, hidden, eta, rnn1, rnn2, dropout, layers]
